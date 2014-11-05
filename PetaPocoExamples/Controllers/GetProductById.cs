2c8e474f066df960dbec934899b2f0b195320263
Nate <nathanj.peterson@gmail.com>
2014-11-04 23:00:39 -0500

    provide example that shows different way queries can be written

--- PetaPocoExamples/Controllers/GetProductById.cs
+++ PetaPocoExamples/Controllers/GetProductById.cs
@@ -8,6 +8,8 @@ namespace PetaPocoExamples.Controllers
         {
             var db = new PetaPoco.Database("example");
             var product = db.SingleOrDefault<Product>("WHERE ProductId = @0", id);
+
+            //// var product = db.SingleOrDefault<Product>("select * from WHERE ProductId = @0", id);
             return product;
         }
     }
