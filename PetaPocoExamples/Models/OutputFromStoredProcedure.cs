b38bd11ca551133bcfcc37f5c90370427bd0f697
Nate <nathanj.peterson@gmail.com>
2014-11-11 08:44:50 -0500

    provide an example for getting output from a stored procedure

---
+++ PetaPocoExamples/Models/OutputFromStoredProcedure.cs
@@ -0,0 +1,18 @@
+﻿using PetaPoco;
+
+namespace PetaPocoExamples.Models
+{
+    public class OutputFromStoredProcedure
+    {
+        // This is not wired up to anything it's just used to explain how to get output params from a stored procedure
+
+        public void Execute(Database db)
+        {
+            var sql = new Sql().Append("declare @@returnParamter int;")
+                .Append(string.Format("exec @@returnParameter = dbo.[procName] '{0}', '{1}';", "param1", "param2"))
+                .Append("select @@returnParamter");
+
+            var returnValue = db.ExecuteScalar<int>(sql);
+        }
+    }
+}
